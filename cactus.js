55e3198b9cc5c96d4e6788581db9d215deb6c05e
TaxEvasion1452 <100240280+TaxEvasion1452@users.noreply.github.com>
2022-02-22 16:32:02 -0500

    Add files via upload

---
+++ cactus.js
@@ -0,0 +1,17 @@
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+
+const commands = [{
+    name: 'sus',
+    description: "Proves this bot isn't sus"
+  }]; 
+
+  client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+  
+    if (interaction.commandName === 'sus') {
+      await interaction.reply("Proof this isn't sus: ");
+    }
+  });
+
+Token = "Lol_noob_you_fucking_thought_I_was_that_stupid_to_leave_token_public?"
