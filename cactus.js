aebd6e206f8d08acd87b84d7f2d5fa27173ce374
TaxEvasion1452 <100240280+TaxEvasion1452@users.noreply.github.com>
2022-02-22 17:14:39 -0500

    Update cactus.js

--- cactus.js
+++ cactus.js
@@ -1,17 +1,31 @@
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 
+client.login('Lol no');
+// this be the bot's token. very important don't forget
+
 const commands = [{
     name: 'sus',
     description: "Proves this bot isn't sus"
   }]; 
-
+// This command is just sending a link to a github page with this code
   client.on('interactionCreate', async interaction => {
     if (!interaction.isCommand()) return;
   
     if (interaction.commandName === 'sus') {
       await interaction.reply("Proof this isn't sus: https://github.com/TaxEvasion1452/TaxEvasion1452/blob/Funni-Cactus-Discord-Bot/cactus.js");
     }
-  });
+});
 
-Token = "Lol_noob_you_fucking_thought_I_was_that_stupid_to_leave_token_public?"
+const comamnds = [{
+    name: "Cactus",
+    description: "Cactus"
+  }]
+// Makes the bot reply
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand()) return;
+
+  if (interaction.commandName === 'Cactus') {
+    await interaction.reply ("Cactus :thumbsup:")  
+
+}})
